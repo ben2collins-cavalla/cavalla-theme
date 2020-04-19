297b2051e472a761b7769ea4b9c350fd84c20ac0
Ben Collins <ben.collins@howard.edu>
2020-04-19 02:21:56 -0400

    updated cta template with new inline attributes

--- src/js/script.js
+++ src/js/script.js
@@ -99,30 +99,30 @@ jQuery.noConflict();
 })(jQuery);
 
 
-var $=jQuery.noConflict();
-$(document).ready(function($){
-   //Play intro on page load
+// var $=jQuery.noConflict();
+// $(document).ready(function($){
+//    //Play intro on page load
 
 
-if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
-	$("video").ready(function () {
-		//window.setTimeout(function(){
-			$("video[autoplay]").each(function(){ this.play(); });
-		//}, 1000);
-	});
-}
+// if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
+// 	$("video").ready(function () {
+// 		//window.setTimeout(function(){
+// 			$("video[autoplay]").each(function(){ this.play(); });
+// 		//}, 1000);
+// 	});
+// }
 
-else{
+// else{
 
-	if ($("video")){
-		$("video[autoplay]").each(function(){ this.play(); });
-		$('video').attr('playsinline', '');
-		$('video').attr('muted', '');
-	}
+// 	if ($("video")){
+// 		$("video[autoplay]").each(function(){ this.play(); });
+// 		$('video').attr('playsinline', '');
+// 		$('video').attr('muted', '');
+// 	}
 
-}
+// }
 
-});
+// });
 
 
 /**** Menu ****/
@@ -135,3 +135,49 @@ jQuery.noConflict();
 	});
 
 })(jQuery);
+
+
+	var elmnt = document.querySelector('.call_to_action_bundle .video');
+	var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+		  videoId: elmnt.getAttribute('data-video-id'),
+		  playerVars: { 'autoplay': 1, 'controls': 0 },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+		event.target.playVideo();
+		event.target.mute();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+
+
+	  }
+
+	  function nextVideo() {
+        player.nextVideo(loop);
+	  }
+  /*     function stopVideo() {
+        player.stopVideo();
+	  } */
